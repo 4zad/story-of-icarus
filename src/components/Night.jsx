2164104b9aeae0445129041e980208301574b19c
NessaLiu <liu_vanessa@rocketmail.com>
2022-07-06 18:46:50 -0400

    fix: fixed sea svg positioning

--- src/components/Night.jsx
+++ src/components/Night.jsx
@@ -178,8 +178,20 @@ function Night() {
             <p ref={titleTextRef}>Landscape with the Fall of Icarus</p>
             <p ref={authorRef}>A Poem by William Carlos Williams</p>
           </div>
-          <SeaTop color1={nightColours[1]} color2={nightColours[0]} />
-          <SeaBottom color1={nightColours[1]} color2={nightColours[0]} />
+          <div className="seaTopContainer">
+            <SeaTop
+              className="seaTop"
+              color1={nightColours[1]}
+              color2={nightColours[0]}
+            />
+          </div>
+          <div className="seaBottomContainer">
+            <SeaBottom
+              className="seaBottom"
+              color1={nightColours[1]}
+              color2={nightColours[0]}
+            />
+          </div>
         </div>
         <div
           ref={treeRef}
