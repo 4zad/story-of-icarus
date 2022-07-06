f67bfdd966b33341cf5dd3be4b7e11380906b848
NessaLiu <liu_vanessa@rocketmail.com>
2022-07-06 15:57:57 -0400

    feature: created new components for sea svg, added poem text and typography

--- src/components/Night.jsx
+++ src/components/Night.jsx
@@ -1,34 +1,100 @@
 import React from "react";
+import { useEffect, useRef } from "react";
+import { ReactComponent as SeaTop } from "../assets/seaTop.svg";
+import { ReactComponent as SeaBottom } from "../assets/seaBottom.svg";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import "./Night.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerEffect({
+  name: "poemTextFade",
+  effect: (targets, config) => {
+    return gsap.from(targets, { autoAlpha: 0 });
+  },
+});
+
 function Night() {
-  const nightColours = ["#0C78C0", "#014C8D", "#503F90", "#3F3684", "#2F2D3A"];
+  const nightColours = ["#0F90E6", "#01417A", "#503F90", "#3F3684", "#2F2D3A"];
+  const titleTextRef = useRef(null);
+
+  useEffect(() => {});
 
   return (
-    <div>
-      <svg
-        width="100%"
-        height="100%"
-        id="svg"
-        viewBox="0 0 1440 400"
-        xmlns="http://www.w3.org/2000/svg"
-        class="transition duration-300 ease-in-out delay-150"
-      >
-        <defs>
-          <linearGradient id="gradient" x1="0%" y1="50%" x2="100%" y2="50%">
-            <stop offset="5%" stop-color="#0c78c0ff"></stop>
-            <stop offset="95%" stop-color="#014c8dff"></stop>
-          </linearGradient>
-        </defs>
-        <path
-          d="M 0,400 C 0,400 0,200 0,200 C 152.53333333333336,202.53333333333333 305.0666666666667,205.06666666666666 471,219 C 636.9333333333333,232.93333333333334 816.2666666666667,258.26666666666665 980,257 C 1143.7333333333333,255.73333333333332 1291.8666666666668,227.86666666666667 1440,200 C 1440,200 1440,400 1440,400 Z"
-          stroke="none"
-          stroke-width="0"
-          fill="url(#gradient)"
-          class="transition-all duration-300 ease-in-out delay-150 path-0"
-        ></path>
-      </svg>
+    <div className="Night">
+      <section className="section" id="page1">
+        <div className="poemTitleContainer">
+          <div ref={titleTextRef} className="titleText">
+            <p>Landscape with the Fall of Icarus</p>
+            <p>A Poem by William Carlos Williams</p>
+          </div>
+          <SeaTop>
+            2 color1={nightColours[0]}1 nightColours[0] color1={}
+            ta s
+            <param name="color1" value={nightColours[0]} />
+            <param name="color2" value={nightColours[1]} />
+          </SeaTop>
+          {/* <SeaBottom>
+            <param name="color3" value={nightColours[0]} />
+            <param name="color4" value={nightColours[1]} />
+          </SeaBottom> */}
+          {/* <svg src="../assets/forest.svg"></svg> */}
+          {/* <svg src="../assets/sea.svg"></svg> */}
+        </div>
+      </section>
+      <section className="section" id="page2">
+        <div className="poemText">
+          <p>According to Brueghel</p>
+          <p>when Icarus fell</p>
+          <p>it was spring</p>
+        </div>
+      </section>
+      <section className="section" id="page3">
+        <div className="poemText">
+          <div className="poemText3-1">
+            <p>a farmer was ploughing</p>
+            <p>his field</p>
+            <p>the whole pageantry</p>
+          </div>
+          <br />
+          <br />
+          <div className="poemText3-2">
+            <p>of the year was</p>
+            <p>awake tingling</p>
+            <p>near</p>
+          </div>
+        </div>
+      </section>
+      <section className="section" id="page4">
+        <div className="poemText">
+          <p>the edge of the sea</p>
+          <p>concerned</p>
+          <p>with itself</p>
+        </div>
+      </section>
+      <section className="section" id="page5">
+        <div className="poemText">
+          <p>sweating in the sun</p>
+          <p>that melted</p>
+          <p>the wings' wax</p>
+        </div>
+      </section>
+      <section className="section" id="page6">
+        <div className="poemText">
+          <p>unsignificantly</p>
+          <p>off the coast</p>
+          <p>there was</p>
+        </div>
+      </section>
+      <section className="section" id="page7">
+        <div className="poemText">
+          <p>a splash quite unnoticed</p>
+          <p>this was</p>
+          <p>Icarus drowning</p>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/svgComponents/SeaBottom.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SeaBottom(props) {
+  return <div>SeaBottom</div>;
+}
+
+export default SeaBottom;
