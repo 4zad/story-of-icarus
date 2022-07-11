88c9c62a09d2594c1582e3224f6329d8a503a42d
NessaLiu <liu_vanessa@rocketmail.com>
2022-07-11 12:12:36 -0400

    chore: cleaned up animation code

--- src/components/Night.jsx
+++ src/components/Night.jsx
@@ -58,67 +58,58 @@ function Night() {
         "<0.25"
       );
 
-      // Title page parallax scrolling
-      const title_para_tl = gsap.timeline({scrollTrigger: {
+    // Title page parallax scrolling
+    const title_para_tl = gsap.timeline({
+      defaults: { overwrite: true },
+      scrollTrigger: {
         trigger: ".headerSection",
         start: "top top",
         end: "bottom top",
         scrub: true,
-        markers: true
-      }});
+        markers: true,
+      },
+    });
 
-      gsap.utils.toArray(".titleParallax").forEach(titleLayer => {
-        const depth = titleLayer.dataset.depth;
-        const movement = -(titleLayer.offsetHeight * depth)
-        title_para_tl.to(titleLayer, {y: movement, ease: "none"}, 0)
-      })
-      // title_para_tl.to(treeRef.current, {y: -75}, 0)
-      // .to(".seaTopContainer", {y: -100}, 0)
-      // .to(".seaBottomContainer", {y: -175}, 0)
+    gsap.utils.toArray(".titleParallax").forEach((titleLayer) => {
+      const depth = titleLayer.dataset.depth;
+      const movement = -(titleLayer.offsetHeight * depth);
+      title_para_tl.to(titleLayer, { y: movement, ease: "none" }, 0);
+    });
+    // title_para_tl.to(treeRef.current, {y: -75}, 0)
+    // .to(".seaTopContainer", {y: -100}, 0)
+    // .to(".seaBottomContainer", {y: -175}, 0)
 
-      // Title page parallax scrolling
-      const rock_para_tl = gsap.timeline({scrollTrigger: {
-        trigger: "#page6",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-        markers: true
-      }});
-
-      // Page 5 - wing animation
-      const wings_tl = gsap.timeline()
+    // Page 5 - wing animation
+    const wings_tl = gsap
+      .timeline({ defaults: { overwrite: true } })
       .to(".wings", {
         scaleX: 1.2,
         // transformOrigin: "center center",
         translateY: 20,
         duration: 1,
         yoyo: true,
-        repeat: -1
-      })
+        repeat: -1,
+      });
 
+    // Page 6 - rock parallax
+    const rock_para_tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#page6",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        markers: true,
+      },
+    });
+    gsap.utils.toArray(".rock").forEach((rockLayer) => {
+      const depth = rockLayer.dataset.depth;
+      const movement = -(rockLayer.offsetHeight * depth);
+      rock_para_tl.to(rockLayer, { y: movement, ease: "none" }, 0);
+    });
 
-      // Page 6 - rock parallax
-      gsap.utils.toArray(".rock").forEach(rockLayer => {
-        const depth = rockLayer.dataset.depth;
-        const movement = -(rockLayer.offsetHeight * depth);
-
-        // console.log(depth);
-        // console.log(movement);
-        rock_para_tl.to(rockLayer, {y: movement, ease: "none"}, 0)
-        // rockMovements.push(movement);
-      })
-      // rock_para_tl.fromTo(".rock1", {y:0}, {y: rockMovements[2], ease: "none"}, 0)
-      // .fromTo(".rock2", {y:0}, {y: rockMovements[1], ease: "none"}, 0)
-      // .fromTo(".rock3", {y:0}, {y: rockMovements[0], ease: "none"}, 0)
-
-      // Increase height of block colour
-      // gsap.set(".pg6BlockColour", {transformOrigin: "bottom center"});
-      // rock_para_tl.to(".pg6BlockColour", {scaleY: 3.5}, 0);
-
-
-      // rock_para_tl.to(".rock1", {y: -75}, 0)
-      // .to(".rock2", {y: -100}, 0)
-      // .to(".rock3", {y: -175}, 0)
+    // Increase height of block colour
+    // gsap.set(".pg6BlockColour", {transformOrigin: "bottom center"});
+    // rock_para_tl.to(".pg6BlockColour", {scaleY: 3.5}, 0);
 
     // poem text fade-in animation
     sections.forEach((section) => {
@@ -155,12 +146,16 @@ function Night() {
             <p ref={authorRef}>A Poem by William Carlos Williams</p>
           </div>
         </div>
-        <div ref={treeRef} className="treesBg titleParallax" data-depth='0.1'></div>
-        <div className="seaContainer" >
-          <div className="seaTopContainer titleParallax" data-depth='0.3'>
+        <div
+          ref={treeRef}
+          className="treesBg titleParallax"
+          data-depth="0.1"
+        ></div>
+        <div className="seaContainer">
+          <div className="seaTopContainer titleParallax" data-depth="0.3">
             <SeaTop color1={nightColours[1]} color2={nightColours[0]} />
           </div>
-          <div className="seaBottomContainer titleParallax" data-depth='0.40'>
+          <div className="seaBottomContainer titleParallax" data-depth="0.40">
             <SeaBottom color1={nightColours[1]} color2={nightColours[0]} />
           </div>
         </div>
@@ -173,7 +168,9 @@ function Night() {
           <p>when Icarus fell</p>
           <p>it was spring</p>
         </div>
-        <div className="treesContainer"><Trees /></div>
+        <div className="treesContainer">
+          <Trees />
+        </div>
       </section>
 
       <section className="section" id="page3">
@@ -220,14 +217,14 @@ function Night() {
         </div>
         <div className="pg6BlockColour"></div>
         <div className="rocksContainer">
-          <div className="rock3container" >
-            <img className="rock rock3" data-depth='0.05' src={rock3} />
+          <div className="rock3container">
+            <img className="rock rock3" data-depth="0.05" src={rock3} />
           </div>
-          <div className="rock2container" >
-            <img className="rock rock2" data-depth='0.2' src={rock2} />
+          <div className="rock2container">
+            <img className="rock rock2" data-depth="0.2" src={rock2} />
           </div>
-          <div className="rock1container" >
-            <img className="rock rock1" data-depth='0.4' src={rock1} />
+          <div className="rock1container">
+            <img className="rock rock1" data-depth="0.4" src={rock1} />
           </div>
         </div>
       </section>
